a44d6ea1b2785cc02ba974af092696f7bfb3c64a
hansen <hendycayen@gmail.com>
2023-02-10 20:43:41 +0700

    configurasi dan desain tabel

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+        'photo'
     ];
 
     /**
@@ -41,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function attendance()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
